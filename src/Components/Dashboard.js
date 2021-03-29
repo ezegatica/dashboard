ab37457cd2ceace9060833a3be79f479d723cf2b
Eze Gatica <ezequielgatica@gmail.com>
2021-03-29 11:53:37 -0300

    responsiveness en celular

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import {
-    Container, Form, Col, Button, Table
-    , Navbar, Nav
+    Container, Form, Col, Button
+    , Navbar, Nav, Alert, Badge
+    , Table
 } from "react-bootstrap";
 // import { NavLink } from "react-router-dom";
 import axios from 'axios'
@@ -19,7 +20,7 @@ export class Dashboard extends Component {
             destino: this.state.destino,
             tag: this.state.tag
         })
-        this.setState({destino: '',tag:''})
+        this.setState({ destino: '', tag: '' })
         this.copyShortToClipboard(res.data.tag)
         swal("URL Creada", `${process.env.REACT_APP_API_URL}${res.data.tag}`, "success");
         this.getUrls()
@@ -62,6 +63,9 @@ export class Dashboard extends Component {
                     </Nav>
                 </Navbar>
 
+                {/* <div class="d-lg-none">PANTALLA CHICA</div>
+                <div class="d-none d-lg-block">PANTALLA GRANDEEEE</div>
+                 */}
                 <Container style={{ background: 'white' }} className="centered p-3 rounded" >
                     <Form onSubmit={this.submit} >
                         <Form.Row onSubmit={() => console.log("b")}>
@@ -69,7 +73,7 @@ export class Dashboard extends Component {
                                 <Form.Control placeholder="URL Larga" required type="url" spellCheck onChange={(e) => this.setState({ destino: e.target.value })} value={this.state.destino} />
                             </Col>
                             <Col xs={4} xl={3} md={3} lg={2}>
-                                <Form.Control placeholder="Tag (opcional)" onChange={(e) => this.setState({ tag: e.target.value })} value={this.state.tag}/>
+                                <Form.Control placeholder="Tag (opcional)" onChange={(e) => this.setState({ tag: e.target.value })} value={this.state.tag} />
                             </Col>
                             <Col>
                                 <Button variant="outline-dark" type="submit">Enviar</Button>
@@ -77,31 +81,56 @@ export class Dashboard extends Component {
                         </Form.Row>
                     </Form>
                     <br />
-                    <Table striped bordered hover responsive>
-                        <thead>
-                            <tr>
-                                <th>Destino</th>
-                                <th>URL Acortada</th>
-                                <th>Visitas</th>
-                                <th>Acciones</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {this.state.urls.map((url) => {
-                                return (
-                                    <tr key={url._id}>
-                                        <td><a href={url.destino}>{url.destino}</a></td>
-                                        <td><a href={`${process.env.REACT_APP_API_URL}${url.tag}`} target="_blank" rel="noreferrer">{process.env.REACT_APP_API_URL}{url.tag}</a></td>
-                                        <td>{url.conteo}</td>
-                                        <td>
-                                            <Button variant="success" onClick={() => this.copyShortToClipboard(url.tag)}>Copy</Button> {' '}
-                                            <Button variant="danger" onClick={() => this.deleteUrl(url._id)}>Delete</Button>
-                                        </td>
-                                    </tr>
-                                )
-                            })}
-                        </tbody>
-                    </Table>
+
+                    {/* PANTALLA GRANDE */}
+                    <div class="d-none d-lg-block">
+                        <Table striped bordered hover responsive>
+                            <thead>
+                                <tr>
+                                    <th>Destino</th>
+                                    <th>URL Acortada</th>
+                                    <th>Visitas</th>
+                                    <th>Acciones</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {this.state.urls.map((url) => {
+                                    return (
+                                        <tr key={url._id}>
+                                            <td><a href={url.destino}>{url.destino}</a></td>
+                                            <td><a href={`${process.env.REACT_APP_API_URL}${url.tag}`} target="_blank" rel="noreferrer">{process.env.REACT_APP_API_URL}{url.tag}</a></td>
+                                            <td>{url.conteo}</td>
+                                            <td>
+                                                <Button variant="success" onClick={() => this.copyShortToClipboard(url.tag)}>Copiar</Button> {' '}
+                                                <Button variant="danger" onClick={() => this.deleteUrl(url._id)}>Borrar</Button>
+                                            </td>
+                                        </tr>
+                                    )
+                                })}
+                            </tbody>
+                        </Table>
+                    </div>
+
+                    {/* PANTALLA CHICA */}
+                    <div class="d-lg-none">
+                        {this.state.urls.map((url) => {
+                            return (
+                                <Alert variant="secondary">
+                                    <p>
+                                        <Alert.Link href={url.destino}>{url.destino}</Alert.Link><br />
+                                        <a href={`${process.env.REACT_APP_API_URL}${url.tag}`} target="_blank" rel="noreferrer">{process.env.REACT_APP_API_URL}{url.tag} </a><Badge pill variant="light">{url.conteo} clicks</Badge>
+                                    </p>
+
+
+                                    <hr />
+                                    <Button variant="success" onClick={() => this.copyShortToClipboard(url.tag)}>Copiar</Button> {' '}
+                                    <Button variant="danger" onClick={() => this.deleteUrl(url._id)}>Borrar</Button>
+                                </Alert>
+                            )
+                        })}
+                    </div>
+
+
                 </Container>
             </>
         )
