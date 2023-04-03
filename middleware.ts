5b67c974380b220c8f4f38a5a58d0af36d4c2747
Eze Gatica <35713330+ezegatica@users.noreply.github.com>
2023-04-03 13:06:57 -0300

    Agregando validacion de token antes de entrar al dashboard

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,14 @@ export async function middleware(request: NextRequest) {
   // Dashboard force login middleware
   if (request.nextUrl.pathname.startsWith('/dashboard')) {
     if (request.cookies.has('token')) {
-      return NextResponse.next();
+      const isValid = await validateToken(request.cookies.get('token')?.value);
+      if (isValid) {
+        return NextResponse.next();
+      } else {
+        const url = new URL('/login', request.url);
+        url.searchParams.set('next', request.nextUrl.pathname);
+        return NextResponse.redirect(url);
+      }
     }
     const url = new URL('/login', request.url);
     url.searchParams.set('next', request.nextUrl.pathname);
@@ -13,27 +20,36 @@ export async function middleware(request: NextRequest) {
   }
 
   // API not public middleware
-  if (request.nextUrl.pathname.startsWith('/api') && !request.nextUrl.pathname.startsWith('/api/public')) {
+  if (
+    request.nextUrl.pathname.startsWith('/api') &&
+    !request.nextUrl.pathname.startsWith('/api/public')
+  ) {
     if (request.cookies.has('token')) {
-      const response = await fetch(`https://jesse.eze.net.ar/check`, {
-        method: 'POST',
-        headers: {
-          Authorization: `Bearer ${request.cookies.get('token')?.value}`
-        }
-      }).then(res => res.json());
-      if (response.valid) {
+      const isValid = await validateToken(request.cookies.get('token')?.value);
+      if (isValid) {
         return NextResponse.next();
       } else {
         return new NextResponse(JSON.stringify({ error: 'Forbidden' }), {
           status: 403
         });
       }
+    } else {
+      return new NextResponse(JSON.stringify({ error: 'Unauthorized' }), {
+        status: 401
+      });
     }
-    return new NextResponse(JSON.stringify({ error: 'Unauthorized' }), {
-      status: 401
-    });
   }
 
   // Default fallback
   return NextResponse.next();
 }
+
+const validateToken = async (token?: string): Promise<boolean> => {
+  const response = await fetch(`https://jesse.eze.net.ar/check`, {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  }).then(res => res.json());
+  return response.valid;
+};
