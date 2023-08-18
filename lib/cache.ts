c22a26055da0ae2bc6c107f8b67b1c86da284207
Eze Gatica <git@ezegatica.com>
2023-08-17 21:31:57 -0300

    Arreglando errorcitos en cache

--- lib/cache.ts
+++ lib/cache.ts
@@ -3,20 +3,20 @@ import { get } from '@vercel/edge-config';
 export const revalidateRoot = async () => {
   const secret = await get('secret');
   const url = getVentasURL();
-  console.log(`Revalidating root in ${url}`);
+  console.info(`Revalidating root in ${url}`);
 
   const res = await fetch(`${url}/api/revalidate?secret=${secret}`, {
     method: 'POST'
   });
   const json = await res.json();
-  console.log({ res:  json });
+  console.info({ res: json });
   return res;
 };
 
 export const revalidatePath = async (path: string) => {
   const secret = await get('secret');
   const url = getVentasURL();
-  console.log(`Revalidating path ${path} in ${url}`);
+  console.info(`Revalidating path ${path} in ${url}`);
 
   const res = await fetch(
     `${url}/api/revalidate?secret=${secret}&path=${path}`,
@@ -25,13 +25,13 @@ export const revalidatePath = async (path: string) => {
     }
   );
   const json = await res.json();
-  console.log({ res:  json });
+  console.info({ res: json });
 
   return res;
 };
 
 const getVentasURL = () => {
-  switch (process.env.NODE_ENV) {
+  switch (process.env.VERCEL_ENV) {
     case 'development':
       return 'https://ventas.preview.ezegatica.com';
     case 'production':
