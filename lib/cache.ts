aa538ae1a89ffe6db52ad21cbd5744dc9b421a8c
Eze Gatica <git@ezegatica.com>
2025-01-17 15:16:26 -0300

    test reval

--- lib/cache.ts
+++ lib/cache.ts
@@ -16,19 +16,32 @@ export const revalidateRoot = async () => {
 
 export const revalidatePath = async (path: string) => {
   const secret = await get('secret');
+
+  console.log({ secret });
+
   const url = getVentasURL();
   console.info(`Revalidating path ${path} in ${url}`);
 
-  const res = await fetch(
-    `${url}/api/revalidate?secret=${secret}&path="/p/${path}"`,
-    {
-      method: 'POST'
-    }
+  const revalidateUrl = new URL(
+    `${url}/api/revalidate`
   );
+
+  revalidateUrl.searchParams.append('secret', secret?.toString() || '');
+  revalidateUrl.searchParams.append('path', `/p/${path}`);
+
+  console.log({revalidateUrl});
+
+  const res = await fetch(revalidateUrl, {
+    method: 'POST'
+  });
 
   if (!res.ok) {
     console.error({ res });
-    return res;
+    if (res.body) {
+      const text = await res.text();
+      console.error({ res: text });
+    }
+    throw res;
   }
 
   if (res.headers.get('content-type')?.includes('application/json')) {
@@ -55,7 +68,7 @@ export async function revalidateAll() {
   );
   const json = await res.json();
   console.info({ res: json });
-};
+}
 
 const getVentasURL = () => {
   switch (process.env.NEXT_PUBLIC_VERCEL_ENV) {
