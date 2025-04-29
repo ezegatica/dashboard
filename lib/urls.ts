cfb0c2a25f60801fc29f121d753032e30850696f
Eze Gatica <git@ezegatica.com>
2025-04-29 19:01:51 -0300

    Agregando url remota variado del ambiente

--- lib/urls.ts
+++ lib/urls.ts
@@ -5,7 +5,18 @@ export const getShortenerURL = () => {
     case 'production':
       return 'https://eze.net.ar';
     default:
-      return 'http://localhost:8787';
+      return 'http://127.0.0.1:8788';
+  }
+};
+
+export const getAuthURL = () => {
+  switch (process.env.NEXT_PUBLIC_VERCEL_ENV) {
+    case 'development':
+    case 'preview':
+    case 'production':
+      return 'https://sso.eze.net.ar';
+    default:
+      return 'http://127.0.0.1:8787';
   }
 };
 
--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,12 @@
 import { NextResponse } from 'next/server';
 import { NextRequest } from 'next/server';
+import { getAuthURL } from './lib/urls';
 
 export async function middleware(request: NextRequest) {
   // Dashboard force login middleware
   if (request.nextUrl.pathname.startsWith('/dashboard')) {
     if (request.cookies.has('token')) {
-      const isValid = await validateToken(request.cookies.get('token')?.value);
+      const isValid = await validateToken(request.cookies.get('token')?.value!);
       if (isValid) {
         return NextResponse.next();
       } else {
@@ -25,7 +26,7 @@ export async function middleware(request: NextRequest) {
     !request.nextUrl.pathname.startsWith('/api/public')
   ) {
     if (request.cookies.has('token')) {
-      const isValid = await validateToken(request.cookies.get('token')?.value);
+      const isValid = await validateToken(request.cookies.get('token')?.value!);
       if (isValid) {
         return NextResponse.next();
       } else {
@@ -44,12 +45,11 @@ export async function middleware(request: NextRequest) {
   return NextResponse.next();
 }
 
-const validateToken = async (token?: string): Promise<boolean> => {
-  // pasar a .env
-  const returnable = await fetch(`http://127.0.0.1:8787/validate`, {
+const validateToken = async (token: string): Promise<boolean> => {
+  const returnable = await fetch(`${getAuthURL()}/validate?app=dashboard-local`, {
     method: 'POST',
     body: JSON.stringify({
-      token: token
+      token
     }),
     headers: {
       'Content-Type': 'application/json',
